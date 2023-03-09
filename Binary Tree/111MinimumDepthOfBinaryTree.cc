bedce349e464422f8df5cfc2b6ab9d9636cf07e1
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-03-09 13:17:32 -0500

    max/min depths

---
+++ Binary Tree/111MinimumDepthOfBinaryTree.cc
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root == NULL) return 0;
+        else que.push(root);
+        int minDepth = 0;
+        while (!que.empty()) {
+            int size = que.size();
+            minDepth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+                if (!node->left && !node->right) return minDepth;
+            }
+        }
+        return minDepth;
+    }
+};
