e2fae64b64ee415bc794e6a96e07fc9eb61ade3d
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-03-30 18:31:27 -0400

    108

---
+++ Binary Tree/108ConvertSortedArrayToBinarySearchTree.cc
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) return nullptr;
+
+        TreeNode* root = new TreeNode(0);   
+        queue<TreeNode*> nodeQue;       
+        queue<int> leftQue;               
+        queue<int> rightQue;              
+        nodeQue.push(root);                
+        leftQue.push(0);                  
+        rightQue.push(nums.size() - 1);    
+
+        while (!nodeQue.empty()) {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+            int left = leftQue.front(); leftQue.pop();
+            int right = rightQue.front(); rightQue.pop();
+            int mid = left + ((right - left) / 2);
+
+            curNode->val = nums[mid];  
+
+            if (left <= mid - 1) {        
+                curNode->left = new TreeNode(0);
+                nodeQue.push(curNode->left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+
+            if (right >= mid + 1) {     
+                curNode->right = new TreeNode(0);
+                nodeQue.push(curNode->right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
+        return root;
+    }
+};
