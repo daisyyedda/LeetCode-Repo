002eaa409b1291865cdce1c74f0e919f62d3ea34
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-08-21 16:19:40 -0400

    two pointers

--- NeetCode 150/Two Pointers/167TwoSumllInputArrayIsSorted.cpp
+++ NeetCode 150/Two Pointers/167TwoSumllInputArrayIsSorted.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Chun Ye on 2023-08-21.
-//
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int lo = 0;
+        int hi = numbers.size() - 1;
+        while (lo < hi) {
+            if (numbers[lo] + numbers[hi] == target) {
+                return {lo+1, hi+1};
+            } else if (numbers[lo] + numbers[hi] < target) {
+                lo++;
+            } else {
+                hi--;
+            }
+        }
+        return {0, 0};
+    }
+};
