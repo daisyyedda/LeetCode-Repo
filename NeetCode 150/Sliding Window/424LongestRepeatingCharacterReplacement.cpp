7b0fe241391d62454dfeb3d7314a7103374609a2
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-08-24 17:57:36 -0400

    started stack today

--- NeetCode 150/Sliding Window/424LongestRepeatingCharacterReplacement.cpp
+++ NeetCode 150/Sliding Window/424LongestRepeatingCharacterReplacement.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         vector<int> count(26);
-        int maxCount = 0;
-
-        int i = 0;
-        int j = 0;
-
-        int result = 0;
-
+        int maxCount = 0, i = 0, j = 0, result = 0;
         while (j < s.size()) {
             count[s[j] - 'A']++;
+            // maxCount stores the max number of repeating characters
             maxCount = max(maxCount, count[s[j] - 'A']);
+            // need to perform more than k swaps, not feasible, so move the window forward by 1
             if (j - i + 1 - maxCount > k) {
                 count[s[i] - 'A']--;
                 i++;
             }
+            // result stores the max length of the window
             result = max(result, j - i + 1);
             j++;
         }
-
         return result;
     }
 };
-// there are quite a few other cases in which i dont know if
--- NeetCode 150/Sliding Window/76MinimumWindowSubstring.cpp
+++ NeetCode 150/Sliding Window/76MinimumWindowSubstring.cpp
@@ -1,3 +1,37 @@
-//
-// Created by Chun Ye on 2023-08-23.
-//
+class Solution {
+public:
+    // runtime O(m+n), sliding window optimized
+    string minWindow(string s, string t) {
+        unordered_map<char, int> hash;
+        for (int i = 0; i < t.length(); i++) {
+            hash[t[i]]++;
+        }
+        int lo = 0, hi = 0, remaining = t.length();
+        int minStart = 0, minWindow = INT_MAX;
+        while (hi < s.length()) {
+            if (hash[s[hi]] > 0) {
+                remaining--;
+            }
+            hash[s[hi]]--;
+            hi++;
+            // increment lo to shorten the window
+            while (remaining == 0) {
+                if (hi - lo < minWindow) {
+                    minStart = lo;
+                    minWindow = hi - lo;
+                }
+                // increment as s[lo] is no longer in window
+                hash[s[lo]]++;
+                // s[lo] was included in string
+                if (hash[s[lo]] > 0) {
+                    remaining++;
+                }
+                lo++;
+            }
+        }
+        if (minWindow != INT_MAX) {
+            return s.substr(minStart, minWindow);
+        }
+        return "";
+    }
+};
