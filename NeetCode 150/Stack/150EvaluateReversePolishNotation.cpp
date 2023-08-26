86a87f71881c62b4fb326b0ee8a42cf3b6e51c2d
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-08-26 17:27:14 -0400

    stack

--- NeetCode 150/Stack/150EvaluateReversePolishNotation.cpp
+++ NeetCode 150/Stack/150EvaluateReversePolishNotation.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<long long> tok;
-        for (string i : tokens) {
-            int num1, num2;
+        stack<int> st;
+        int num1, num2;
+        for (auto &i : tokens) {
             if (i == "+" || i == "-" || i == "*" || i == "/") {
-                num1 = tok.top();
-                tok.pop();
-                num2 = tok.top();
-                tok.pop();
+                num1 = st.top();
+                st.pop();
+                num2 = st.top();
+                st.pop();
             }
-            if (i == "+") tok.push(num2 + num1);
-            else if (i == "-") tok.push(num2 - num1);
-            else if (i == "*") tok.push(num2 * num1);
-            else if (i == "/") tok.push(num2 / num1);
-            else tok.push(stoll(i));
+            if (i == "+") {
+                st.push(num1+num2);
+            } else if (i == "-") {
+                st.push(num2-num1);
+            } else if (i == "*") {
+                st.push(num1*num2);
+            } else if (i == "/") {
+                st.push(num2/num1);
+            } else {
+                st.push(stoi(i));
+            }
         }
-        return tok.top();
+        return st.top();
     }
 };
