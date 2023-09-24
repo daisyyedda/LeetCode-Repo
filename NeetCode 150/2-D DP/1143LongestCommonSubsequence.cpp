e7fbf6ee635fd6fc00d6c60b0d0554dd74e767da
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-09-24 18:19:03 -0400

    ...

--- NeetCode 150/2-D DP/1143LongestCommonSubsequence.cpp
+++ NeetCode 150/2-D DP/1143LongestCommonSubsequence.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Chun Ye on 2023-09-23.
-//
+/*
+use 2-D DP
+if text1[m] == text2[n], grid[m][n] = 1 + grid[m-1][n-1]
+otherwise, grid[m][n] = max(grid[m-1][n], grid[m][n-1])
+base case is when one string has a length of 0, common subsequence is 0
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        // initialize a grid of 0 to handle base cases implicitly
+        vector<vector<int>> grid(m+1, vector<int>(n+1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i-1] == text2[j-1]) {
+                    grid[i][j] = 1 + grid[i-1][j-1];
+                } else {
+                    grid[i][j] = max(grid[i-1][j], grid[i][j-1]);
+                }
+            }
+        }
+        return grid[m][n];
+    }
+};
--- NeetCode 150/2-D DP/62UniquePaths.cpp
+++ NeetCode 150/2-D DP/62UniquePaths.cpp
@@ -1,3 +1,18 @@
-//
-// Created by Chun Ye on 2023-09-23.
-//
+/*
+this is 2-D Dynamic Programming
+recurrence relation is grid[m][n] = grid[m-1][n] + grid[m][n-1]
+define base cases, since we can only move down or right, the topmost and leftmost rows/columns all have a value of 1.
+*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // initialize a grid of 1 to handle the base cases implicitly
+        vector<vector<int>> matrix(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+            }
+        }
+        return matrix[m-1][n-1];
+    }
+};
